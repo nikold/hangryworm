b71afe44da32c4f8498e98c8b340d18c5c959dcf
dubraver <nikol_dn@list.ru>
2016-02-07 22:00:42 +0400

    123

--- core/src/com/dubraver/game/HungryWorm.java
+++ core/src/com/dubraver/game/HungryWorm.java
@@ -8,12 +8,13 @@ import com.dubraver.game.screens.MenuScreen;
 public class HungryWorm extends Game {
 	public SpriteBatch batch;
 	public BitmapFont font;
-    //test 
+    //t121est  
 	@Override
 	public void create() {
 		
 		batch = new SpriteBatch();
         font = new BitmapFont();
+		//567576
 		setScreen(new MenuScreen(this));
 	}
 }
